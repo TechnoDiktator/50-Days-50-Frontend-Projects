fb32b5c9e11b46947d3335593b2eb23beb7e0249
TechnoDiktator <99278069+TechnoDiktator@users.noreply.github.com>
2023-03-13 19:37:40 +0530

    Add files via upload

---
+++ Project11-incrementcounter/script.js
@@ -0,0 +1,28 @@
+const counters = document.querySelectorAll('.counter')
+
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+
+        const increment = target/500
+
+        console.log(increment)
+
+        if(c<target){
+            counter.innerText = `${Math.ceil(c+ increment)}`
+            setTimeout(updateCounter , 1)
+        
+        }
+    
+    }
+
+
+    updateCounter()
+
+
+})
