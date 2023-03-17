84a94c956dfff76fe39fb3b6e1eafcec8e75ad9e
Tarang Rastogi <99278069+TechnoDiktator@users.noreply.github.com>
2023-03-17 12:23:02 +0530

    Add files via upload

---
+++ Project25 - Instagram Like Feature/script.js
@@ -0,0 +1,64 @@
+const loveMe = document.querySelector('.loveMe')
+const times = document.querySelector('#times')
+
+let clickTime = 0
+let timesClicked = 0
+
+
+loveMe.addEventListener('click'  , (e)=>{
+    
+    if(clickTime === 0){
+        clickTime = new Date().getTime()
+
+    }else {
+
+        if((new Date().getTime()  - clickTime < 800 )){
+
+            createHeart(e)
+            clickTime = 0
+            console.log(123 , 'creating heart')
+        }else{
+            clickTime = new Date().getTime()
+        }
+
+    }
+
+})
+
+
+
+
+const createHeart  =  (e) =>{
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-heart')
+    const x = e.clientX  //absolute coordinates with respect to thje viewport
+    const y = e.clientY
+
+
+    //finding the coordinates relative to the image
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    console.log(xInside , yInside)
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMe.appendChild(heart)
+
+    times.innerHTML = ++timesClicked
+
+
+    setTimeout(() =>{
+        heart.remove()
+    } ,2000)
+
+
+}
+
+
+
